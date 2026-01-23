52610789977c07c64d744dc0b14629a40f9aae5e
JXML99 <jamaal.haji99@gmail.com>
2026-01-23 09:10:23 +0000

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
     const API_KEY = 'f1d96853b6b649c59b823a069b1d7eb8';
     
     const BLOCKED_SOURCES = [
@@ -74,4 +74,4 @@ module.exports = async function handler(req, res) {
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-};
+}
